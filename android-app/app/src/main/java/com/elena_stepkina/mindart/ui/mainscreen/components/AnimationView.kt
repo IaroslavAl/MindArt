178458598a1248f4142c56d89f89f3d95e969f62
IaroslavAl <Iaroslav.aleksandrovich.beldin@gmail.com>
2025-06-13 09:51:42 +0300

    [Android] fix loading animation

--- android-app/app/src/main/java/com/elena_stepkina/mindart/ui/mainscreen/components/AnimationView.kt
+++ android-app/app/src/main/java/com/elena_stepkina/mindart/ui/mainscreen/components/AnimationView.kt
@@ -1,6 +1,9 @@
 package com.elena_stepkina.mindart.ui.mainscreen.components
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.net.toUri
 import coil.compose.AsyncImage
@@ -9,15 +12,20 @@ import coil.request.ImageRequest
 import com.elena_stepkina.mindart.R
 
 @Composable
-fun AnimationView() {
+fun AnimationView(modifier: Modifier = Modifier) {
     val context = LocalContext.current
     val rawUri = "android.resource://${context.packageName}/${R.raw.loading}".toUri()
 
-    AsyncImage(
-        model = ImageRequest.Builder(context)
-            .data(rawUri)
-            .decoderFactory(GifDecoder.Factory())
-            .build(),
-        contentDescription = "GIF from raw"
-    )
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(context)
+                .data(rawUri)
+                .decoderFactory(GifDecoder.Factory())
+                .build(),
+            contentDescription = "GIF from raw"
+        )
+    }
 }
--- android-app/app/src/main/java/com/elena_stepkina/mindart/ui/mainscreen/screenStates/ResultScreen.kt
+++ android-app/app/src/main/java/com/elena_stepkina/mindart/ui/mainscreen/screenStates/ResultScreen.kt
@@ -1,6 +1,6 @@
 package com.elena_stepkina.mindart.ui.mainscreen.screenStates
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -12,7 +12,6 @@ import androidx.compose.ui.unit.dp
 import com.elena_stepkina.mindart.R
 import com.elena_stepkina.mindart.ui.mainscreen.MainViewModel
 import com.elena_stepkina.mindart.ui.mainscreen.components.AnimationView
-import com.elena_stepkina.mindart.ui.mainscreen.components.CustomButton
 import com.elena_stepkina.mindart.ui.mainscreen.components.TextView
 
 @Composable
@@ -20,27 +19,12 @@ fun ResultScreen(viewModel: MainViewModel) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(horizontal = 32.dp),
+            .padding(horizontal = 32.dp)
+            .padding(bottom = 32.dp),
+        verticalArrangement = Arrangement.SpaceBetween,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Box(
-            modifier = Modifier.weight(1f),
-            contentAlignment = Alignment.Center
-        ) {
-            AnimationView()
-        }
-        Box(modifier = Modifier.padding(bottom = 32.dp)) {
-            TextView(stringResource(R.string.result_image_creating))
-//            CustomButton(
-//                text = stringResource(R.string.result_download),
-//                modifier = Modifier.padding(bottom = 16.dp),
-//                onClick = { viewModel.goToNext() }
-//            )
-//            CustomButton(
-//                text = stringResource(R.string.result_repeat),
-//                modifier = Modifier.padding(bottom = 32.dp),
-//                onClick = { viewModel.goToNext() }
-//            )
-        }
+        AnimationView(modifier = Modifier.weight(1f))
+        TextView(stringResource(R.string.result_image_creating))
     }
 }
